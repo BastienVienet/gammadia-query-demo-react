1fdc9d175aa0be149b9e2732e5a3ba422c646eb2
BastienVienet <bastien.vienet@outlook.com>
2022-12-23 09:53:00 +0100

    Project Setup

---
+++ src/components/RQSuperHeroes.page.tsx
@@ -0,0 +1,7 @@
+export const RQSuperHeroesPage = () => {
+    return (
+        <div>
+            RQ Super Heroes Page
+        </div>
+    )
+}
---
+++ src/components/SuperHeroes.page.tsx
@@ -0,0 +1,27 @@
+import {useState, useEffect} from 'react'
+import axios, {AxiosResponse} from 'axios'
+
+export const SuperHeroesPage = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [data, setData] = useState<Array<{ name: string }>>([])
+
+    useEffect(() => {
+        axios.get<Array<{ name: string }>>('http://localhost:4000/superheroes').then((res: AxiosResponse<Array<{ name: string }>>) => {
+            setData(res.data)
+            setIsLoading(false)
+        })
+    }, [])
+
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+
+    return (
+        <>
+            <h2>Super Heroes Page</h2>
+            {data.map(hero => {
+                return <div>{hero.name}</div>
+            })}
+        </>
+    )
+}

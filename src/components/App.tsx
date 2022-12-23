9960dee5bd0f6c995c1d9f059d9121db3d2724a8
BastienVienet <bastien.vienet@outlook.com>
2022-12-23 13:48:52 +0100

    Adding Query Client Provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import {QueryClientProvider, QueryClient} from 'react-query'
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from 'react-query'
+import {ReactQueryDevtools} from "react-query/devtools";
 import '../style/App.css'
 import {HomePage} from './Home.page'
 import {RQSuperHeroesPage} from './RQSuperHeroes.page'
@@ -10,29 +11,30 @@ const queryClient = new QueryClient()
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
-        <Router>
-            <>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/super-heroes'>Traditional Super Heroes</Link>
-                        </li>
-                        <li>
-                            <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Routes>
-                    <Route path='/' element={<HomePage/>}/>
-                    <Route path='/super-heroes' element={<SuperHeroesPage/>}/>
-                    <Route path='/rq-super-heroes' element={<RQSuperHeroesPage/>}/>
-                </Routes>
-            </>
-        </Router>
-    </QueryClientProvider>
+            <Router>
+                <>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+                            </li>
+                            <li>
+                                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route path='/' element={<HomePage/>}/>
+                        <Route path='/super-heroes' element={<SuperHeroesPage/>}/>
+                        <Route path='/rq-super-heroes' element={<RQSuperHeroesPage/>}/>
+                    </Routes>
+                </>
+            </Router>
+            <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
+        </QueryClientProvider>
     )
 }
 
